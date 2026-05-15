fa1cf0aca9d557b550d764948a221a19f6c9253b
Allo Olorunfemi <alloolorunfemi@gmail.com>
2026-05-15 14:44:29 +0100

    slight update

--- src/main.tsx
+++ src/main.tsx
@@ -10,18 +10,13 @@ Sentry.init({
   dsn: "https://e9a7854577b06321a29e8f546776b0f9@o4511149679116288.ingest.de.sentry.io/4511393309524048",
   // Setting this option to true will send default PII data to Sentry.
   // For example, automatic IP address collection on events
-  sendDefaultPii: true
+  sendDefaultPii: true,
+    tracesSampleRate: 1.0,
+
 });
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <button
-      onClick={() => {
-        throw new Error('This is your first error!');
-      }}
-    >
-      Break the world
-    </button>
     <Toaster />
   </StrictMode>,
 )
